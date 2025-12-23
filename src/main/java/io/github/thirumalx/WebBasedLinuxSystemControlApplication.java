aa9709aa6af880e0d12b05c371084e1f9934934f
Thirumal M <racetortoise@gmail.com>
2025-12-23 12:07:56 +0530

    first commit

---
+++ src/main/java/io/github/thirumalx/WebBasedLinuxSystemControlApplication.java
@@ -0,0 +1,13 @@
+package io.github.thirumalx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBasedLinuxSystemControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBasedLinuxSystemControlApplication.class, args);
+	}
+
+}
